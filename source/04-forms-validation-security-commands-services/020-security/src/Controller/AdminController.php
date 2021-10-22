3886e50f6a3d16af4d75df5f4b4e609f6c26b195
Artur Karczmarczyk <artur@ideaspot.pl>
2021-10-22 23:35:23 +0200

    WiP lecture 4 security init

--- source/04-forms-validation-security-commands-services/020-security/src/Controller/AdminController.php
+++ source/04-forms-validation-security-commands-services/020-security/src/Controller/AdminController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
+        /** @var User $user */
+        $user = $this->getUser();
+
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'user' => $user,
         ]);
     }
 }
