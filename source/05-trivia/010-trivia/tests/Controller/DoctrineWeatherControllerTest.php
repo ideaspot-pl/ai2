a009be2069ce08143d0e49ac802022473d694566
Artur Karczmarczyk <artur@ideaspot.pl>
2021-11-07 23:00:20 +0100

    lecture 05

---
+++ source/05-trivia/010-trivia/tests/Controller/DoctrineWeatherControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DoctrineWeatherControllerTest extends WebTestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true); // @todo tutaj!
+    }
+}
