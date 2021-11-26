132d7f82015b8101810edc7643e06d623e72fe32
Artur Karczmarczyk <artur@ideaspot.pl>
2021-11-26 13:44:41 +0100

    maven container (lab08)

---
+++ docker/ai2-maven/maven.sh
@@ -0,0 +1,5 @@
+# Apache Maven Environmental Variables
+# MAVEN_HOME for Maven 1 - M2_HOME for Maven 2
+export JAVA_HOME=/usr/lib/jvm/jre-openjdk
+export M2_HOME=/opt/maven
+export PATH=${M2_HOME}/bin:${PATH}
